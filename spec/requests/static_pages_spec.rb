84bf89177fc204af29b68f0d394774f42c40117f
HuberyDu <duxiaolong92@gmail.com>
2013-11-04 15:35:55 +0800

    Finish layout and routes

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -1,30 +1,32 @@
 require 'spec_helper'
 
 describe "StaticPages" do
-  
+  subject {page}
+
   describe "Home Page" do
-  	it "Should have the content 'Sample app'"do
-      visit "/static_pages/home"
-      expect(page).to have_content("Sample app")
-    end
-
-    it "Shoud have the title 'Home'" do
-      visit "/static_pages/home"
-      expect(page).to have_title("Home")
-    end
+    before {visit root_path}
+    it{should have_content('Sample app')}
+    it{should have_title('m')}
   end
 
-  describe "Help Page" do
-  	it "Should have the content 'Help" do
-  	  visit '/static_pages/help'
-  	  expect(page).to have_content("Help")
-  	end
-  end
+  # describe "Help Page" do
+  # 	it "Should have the content 'Help" do
+  # 	  visit help_path
+  # 	  expect(page).to have_content("Help")
+  # 	end
+  # end
 
-  describe "About Page" do
-  	it "Should have the content 'About'" do
-  	  visit '/static_pages/about'
-  	  expect(page).to have_content("About")
-  	end
-  end
+  # describe "About Page" do
+  # 	it "Should have the content 'About'" do
+  # 	  visit about_path
+  # 	  expect(page).to have_content("About")
+  # 	end
+  # end
+
+  # describe "Contact Page" do
+  #   it "Should have the content 'Contact'" do
+  #     visit contact_path
+  #     expect(page).to have_content("Contact")
+  #   end
+  # end
 end
