dc16828bec00746cfde24158062e4a4f3e85e084
HuberyDu <duxiaolong92@gmail.com>
2013-11-12 07:45:34 +0800

    updation-users

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -18,6 +18,18 @@ class UsersController < ApplicationController
   	end
   end
 
+  def update
+    @user = User.find(params[:id])
+    if @user.update_attribute(params[:user])
+      redirect_to root_path
+    else
+      render "edit"
+    end
+  end
+
+  def edit
+    @user = User.find(params[:id])
+  end
   private
 
     def user_params
--- spec/requests/user_pages_spec.rb
+++ spec/requests/user_pages_spec.rb
@@ -112,12 +112,29 @@ describe "UserPages" do
       end
 
 
-    end
+    end 
 
     describe "remember_token" do
       before{@user.save}
 
       it{expect(@user.remember_token).not_to be_blank}
+    end
+
+    describe "edit" do
+      let(:user){FactoryGirl.create(user)}
+      before{
+        visit edit_user_path(user)
+      }
+
+      describe "page" do
+        it{should have_link('change', href:"http://gravatar.com/emails")}
+      end
+
+      describe "with invalid information" do
+        before{click_button "Save change"}
+
+        it{should have_content("error")}
+      end
     end
   end
 end
